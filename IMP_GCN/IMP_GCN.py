00bb2386fa8b8356df8ab01b1addd3d85f257d9f
ZhaolinGao <zhaolin.gao@mail.utoronto.ca>
2021-07-30 16:54:12 -0400

    Set hard limit on GPU

--- IMP_GCN/IMP_GCN.py
+++ IMP_GCN/IMP_GCN.py
@@ -405,7 +405,7 @@ if __name__ == '__main__':
     # # )
     # config.gpu_options.allow_growth = True
     # sess = tf.Session(config=config)
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
     sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
     """
